f895a8bc67dc1f6ba2fcedb58b73a19cc5cf8cf7
Corey Ferguson <corey.t.ferguson@gmail.com>
2014-07-13 15:12:15 -0700

    Setting active profile in root context instead of servlet context.

--- src/main/java/com/example/config/WebAppInitializer.java
+++ src/main/java/com/example/config/WebAppInitializer.java
@@ -1,6 +1,11 @@
 package com.example.config;
 
+import org.springframework.core.env.AbstractEnvironment;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.support.ResourcePropertySource;
 import org.springframework.util.ObjectUtils;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
@@ -9,21 +14,29 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 import javax.servlet.Filter;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import java.io.IOException;
 
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
     /**
-     * Overriding createServletApplicationContext() to include setting active profile.
+     * Overriding to include setting active profile.
      */
     @Override
-    protected WebApplicationContext createServletApplicationContext() {
-        AnnotationConfigWebApplicationContext servletAppContext = new AnnotationConfigWebApplicationContext();
-        servletAppContext.getEnvironment().setActiveProfiles("preprod");
-        Class<?>[] configClasses = getServletConfigClasses();
+    protected WebApplicationContext createRootApplicationContext() {
+        Class<?>[] configClasses = getRootConfigClasses();
         if (!ObjectUtils.isEmpty(configClasses)) {
-            servletAppContext.register(configClasses);
+            AnnotationConfigWebApplicationContext rootAppContext = new AnnotationConfigWebApplicationContext();
+
+            String[] activeProfiles = getActiveProfiles();
+            rootAppContext.getEnvironment().setActiveProfiles(activeProfiles);
+
+            rootAppContext.register(configClasses);
+            return rootAppContext;
         }
-        return servletAppContext;
+        else {
+            return null;
+        }
     }
 
     @Override
@@ -53,8 +66,16 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
         return new Filter[] {characterEncodingFilter};
     }
 
-//    @Override
-//    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
-//        registration.setInitParameter("spring.profiles.active", "default");
-//    }
+
+    protected String[] getActiveProfiles() {
+        PropertySource propertySource = null;
+        try {
+            propertySource = new ResourcePropertySource("classpath:application.properties");
+            String profilesString = (String) propertySource.getProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME);
+            return profilesString.split(",");
+        } catch (IOException e) {
+            throw new ResourceAccessException("application.properties is not available on the classpath");
+        }
+    }
+
 }
